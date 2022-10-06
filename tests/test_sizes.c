1d978017e33d61d2ba98e38ba512b8c5fa217ce1
Norayr Chilingarian <norayr@arnet.am>
2022-10-07 01:00:56 +0400

    test sizes

---
+++ tests/test_sizes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main()
+{
+  struct dirent *dir;
+  printf ("size of dirent: %ld\n", sizeof(*dir));
+  printf ("size of d_ino: %ld\n", sizeof(dir->d_ino));
+  printf ("size of d_off: %ld\n", sizeof(dir->d_off));
+  printf ("size of d_reclen: %ld\n", sizeof(dir->d_reclen));
+  printf ("size of d_type: %ld\n", sizeof(dir->d_type));
+  printf ("size of d_name: %ld\n", sizeof(dir->d_name));
+  return 0;
+
+}
